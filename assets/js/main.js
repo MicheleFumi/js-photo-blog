789673f46ee102d2706e5ff01d42a9c1fcf27cee
MicheleFumi <michele.fumi96@gmail.com>
2024-10-18 11:41:45 +0200

    add axios cdn

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,3 +13,11 @@ Inseriamo un foglio JavaScript ed effettuiamo una chiamata AJAX all’API di JSO
 Bonus
 rendi la pagina responsive, in modo che su mobile e tablet le foto si dispongano man mano una sotto l’altra ed il titolo abbia una dimensione adeguata
  */
+
+
+let cardEl = document.querySelector(".card")
+
+console.log(cardEl);
+
+
+axios.get("")
