635d2396706dfe92e7c24032b066f4d41cecd7c7
MicheleFumi <michele.fumi96@gmail.com>
2024-10-21 12:08:19 +0200

    miletone 2 fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,13 +18,17 @@ DAY 2
 
 Milestone 1
 Facciamo in modo di creare un overlay che copra l’intera pagina e all’interno, centrata, disponiamo un’immagine qualunque ed un button di chiusura.
+
 Milestone 2
 Facciamo sparire l’overlay con l’aiuto di una classe CSS che imposti il display: none .
+
 Dopodichè facciamo sì che cliccando una qualunque foto. L’overlay ricompaia.
 Cliccando invece il button di chiusura, l’overlay scompare nuovamente.
+
 Milestone 3
 Inseriamo il pezzo di logica finale: quando una foto viene cliccata, dobbiamo fare in modo che sia proprio quella foto a essere mostrata all’interno dell’overlay.
-Ci sono diversi modi di farlo, prova a sperimentare :faccia_leggermente_sorridente:
+
+
 Bonus
 Spostandosi col mouse sopra le foto, queste si zoommano, ruotano di 10 gradi e la loro ombra aumenta, il tutto in manierà fluida. Inoltre il mouse diventa un puntatore, per far capire all’utente che può cliccare
 
@@ -33,6 +37,9 @@ Spostandosi col mouse sopra le foto, queste si zoommano, ruotano di 10 gradi e l
 
 let cardEl = document.getElementById("card-wrap")
 let overlayEl =document.querySelector(".overlay")
+
+
+console.log(overlayEl);
 
 console.log(cardEl);
 
@@ -45,7 +52,8 @@ axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6")
     console.log(cards);
 // creo la variabile di appoggio dove salverò il markup responsive sottoforma di stringa
     let cardElement = ''
-
+    
+    
     
     // creo un ciclo dove per ogni titolo e url creo una card
     cards.forEach(card => {
@@ -56,7 +64,7 @@ axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6")
                    
                     <div class="card">
                      <img src="./assets/img/pin.svg" class="pin">
-                        <img src=${url} class="card-img-top" alt="...">
+                        <img id="card-img" src=${url} class="card-img-top" alt="...">
                         <div class="card-body">
                             <p class="card-text">${title}</p>
                         </div>
@@ -65,9 +73,24 @@ axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6")
         `
         // inserisco le 6 card nella variabile cardElement
         cardElement += markup
+        cardEl.innerHTML= cardElement
+    
+       let cardImgEl = document.getElementById('card-img')
+       console.log(cardImgEl);
+       
+        
+        cardImgEl.addEventListener('click', ()=>{
+            overlayEl.classList.remove("d-none")
+            
+         }) 
+    
     });
     // stampo la variabile con le card responsive
-    cardEl.innerHTML= cardElement
+   
+
+   
 
   })
   .catch(err => console.error(err));
+
+ 
